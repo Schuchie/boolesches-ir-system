b6094f4cd73ad4c7847b0f09dbc4f92aa9dbbb87
Marco <marco.russi97@gmail.com>
2021-05-07 11:26:38 +0200

    ✨ Add new ID.txt generator

--- doc/doc.py
+++ doc/doc.py
@@ -4,3 +4,15 @@ class Document:
         self.url = url.lower()
         self.title = title.lower()
         self.text = text.lower()
+
+    def getID(self):
+        return self.id
+
+    def getURL(self):
+        return self.url
+
+    def getTitle(self):
+        return self.title
+
+    def getText(self):
+        return self.text
--- main.py
+++ main.py
@@ -5,16 +5,32 @@ from io import BytesIO
 from doc import Document
 
 
-def main():
+def read_file_into_docs(file):
     docs = list()
 
-    with open("doc_dump.txt", mode="r", encoding="utf-8") as f:
+    with open(file, mode="r", encoding="utf-8") as f:
         for l in f.readlines():
             data = l.split("\t")
             if len(data) == 4:
                 docs.append(Document(data[0], data[1], data[2], data[3]))
             else:
                 print("NoNoNo")
+    return docs
+
+
+def create_id_file_from_docs(file, docs):
+    with open(file, mode="w", encoding="utf-8") as f:
+        for doc in docs:
+            f.write(doc.getID())
+            f.write("\t")
+            f.write(doc.getTitle())
+            f.write("\n")
+        f.close()
+
+
+def main():
+    docs = read_file_into_docs("doc_dump.txt")
+    create_id_file_from_docs("ID.txt", docs)
 
 
 if __name__ == '__main__':
