30bb888d34003482d707b1c1dcdc3f054b5edebf
Marco <marco.russi97@gmail.com>
2021-05-16 21:30:21 +0200

    ✨ Add NOT operation

--- query/bool_juncture.py
+++ query/bool_juncture.py
@@ -1,3 +1,4 @@
+from doc.doc import Document
 from index.posting_list import PostingList
 from index.indexer import Indexer
 import re
@@ -24,12 +25,21 @@ class BoolJuncture:
             andPostingList = self.merge_and(andPostingList, orPostingList)
         return list(orPostingList.keys())
 
-    def get_posting_list_from_term(self, term):
+    def get_posting_list_from_term(self, term: str):
         # parse previous query results
         if re.match(r"\[.*\]", term.strip(), re.M | re.I):
             l = ast.literal_eval(term.strip())
             return self.list_to_dict(l)
         # regular bool term
+        if term.startswith("NOT"):
+            res = {}
+            strTerm = term.lstrip("NOT").strip()
+            # get all docs without the term
+            for doc in self.indexer.docs:
+                if not self.doc_has_term(doc, strTerm):
+                    res[doc.get_id()] = PostingList(0)
+            return res
+
         dict = self.indexer.get_dictionary(term.strip())
         if dict is None:
             return {}
@@ -55,3 +65,9 @@ class BoolJuncture:
         for l in list:
             out[l] = PostingList(0)
         return out
+
+    def doc_has_term(self, doc: Document, term: str):
+        for t in doc.get_text():
+            if t == term:
+                return True
+        return False
