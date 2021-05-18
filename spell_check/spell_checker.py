c20d3667c058bd766ff5781c18f2063d903234bf
willfri <git@fritzsche.io>
2021-05-18 22:26:10 +0200

    ✨ Add levenshtein distance

--- spell_check/spell_checker.py
+++ spell_check/spell_checker.py
@@ -1,25 +1,50 @@
+import collections
+
 from index.indexer import Indexer
 from .k_gram_indexer import KGramIndexer
-from .utils import jaccard
+from .utils import jaccard, levenshtein_distance
 
 
 class SpellChecker:
     j = None
+    limit = None
     k_gram_indexer = None
 
-    def __init__(self, indexer: Indexer, j=0.5, k=2):
-        self.j = j
+    def __init__(self, indexer: Indexer, j=0.5, k=2, limit=3):
         self.k_gram_indexer = KGramIndexer(indexer, k)
         self.k_gram_indexer.parse()
+        self.j = j
+        self.limit = limit
 
     def check(self, term):
         dictionaries = self.k_gram_indexer.get_dictionaries_for_term(term)
         terms = set()
 
-        for dict in dictionaries:
-            if jaccard(term, dict.term) < self.j:
+        for d in dictionaries:
+            if jaccard(term, d.term) < self.j:
                 continue
 
-            terms.add(dict.term)
+            terms.add(d.term)
 
-        return list(terms)
+        levenshtein_distances = {}
+
+        for t in terms:
+            levenshtein_distances[t] = levenshtein_distance(t, term)
+
+        levenshtein_distances_sorted = collections.OrderedDict(
+            sorted(
+                levenshtein_distances.items(),
+                key=lambda item: item[1],
+            )
+        )
+
+        output = []
+
+        # Limit results
+        for i, (key, value) in enumerate(levenshtein_distances_sorted.items()):
+            if i >= self.limit:
+                break
+
+            output.append(key)
+
+        return output
